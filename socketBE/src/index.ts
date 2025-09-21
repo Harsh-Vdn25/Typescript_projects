0affd8849d9e2cc6129f74e625105768a2174182
Harsh-Vdn25 <hars.vdn16@gmail.com>
2025-09-21 22:53:21 +0530

    Added typesafety room management

--- socketBE/src/index.ts
+++ socketBE/src/index.ts
@@ -1,49 +1,83 @@
-import { WebSocketServer,WebSocket } from "ws";
-import url from 'url';
+import { WebSocketServer, WebSocket } from "ws";
+import url from "url";
 
-const wss=new WebSocketServer({port:3000});
-let userCount=0;
+const wss = new WebSocketServer({ port: 3000 });
+let userCount = 0;
 
 //roomId:string socket:WebSocket
-let allSockets=new Map<string,WebSocket[]>();
+let allSockets = new Map<string, WebSocket[]>();
 
-interface messageType{
-    type:string;
-    message:string;
+type SocketMessage =
+    { type: "join"; payload: { roomId: string } }
+  | { type: "chat"; payload: { message: string } };
+
+//prevents socket duplication in the single room
+function checkSocket(socket:WebSocket,roomId:string){
+    return allSockets.get(roomId)?.includes(socket);
 }
 
-wss.on('connection',(socket,req)=>{
-    socket.on('error',console.error);
 
-    const roomId=req?.url?.split('=')[1]||'';
-    if(!roomId){
-        throw new Error("No room Id");
+wss.on("connection", (socket, req) => {
+  socket.on("error", console.error);
+
+  let currentRoomInfo: string | null = null;
+
+  socket.on("message", (e) => {
+    const message = e.toString();
+    let messageInfo: SocketMessage;
+
+    try {
+      messageInfo = JSON.parse(message);
+    } catch (err) {
+      console.log("Not a Valid info", message);
+      return;
     }
 
-    const roomSockets=allSockets.get(roomId)||[];
-    roomSockets.push(socket);
-    allSockets.set(roomId,roomSockets);
+    if (messageInfo.type === "join") {
+      const roomId = messageInfo.payload?.roomId;
+      if (!roomId) {
+        console.log("No room Id");
+        return;
+      }
 
-    socket.on('message',(e)=>{
-        const message=e.toString();
-        let messageInfo:messageType;
-        try{
-            messageInfo=JSON.parse(message);
-        }catch(err){
-            console.log("Not a Valid info",message);
-            return;
+      
+      if(checkSocket(socket,roomId)){
+        socket.send("You are already part of this room");
+        return;
+      }
+      currentRoomInfo = roomId;
+      const roomSockets = allSockets.get(roomId) || [];//Room with no users 
+      roomSockets.push(socket);
+      allSockets.set(roomId, roomSockets);
+
+    } else if (messageInfo?.type === "chat") {
+
+      if (!currentRoomInfo) {
+        console.log("Not part of any room ");
+        return;
+      }
+
+      const userRoom: WebSocket[] = allSockets.get(currentRoomInfo) || [];
+
+      try {
+        if (userRoom.includes(socket)) {
+          userRoom.map((s) => s.send(messageInfo.payload?.message));
         }
-        if(messageInfo?.type==='chat'){
-            for(const [key,userSocket] of allSockets.entries()){
-           if (userSocket.includes(socket)){
-            userSocket.map(s=>s.send(messageInfo.message));
-           }
-        }
-        }
-    })
-    //Remove all the dead sockets 
-    socket.on("close",()=>{
-        const roomSockets=(allSockets.get(roomId)||[]).filter(x=>x!=socket);
-        allSockets.set(roomId,roomSockets);
-    })
-})
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    }
+  });
+
+  //Remove all the dead sockets
+  socket.on("close", () => {
+    for (const [key, socketList] of allSockets.entries()) {
+      if (socketList.includes(socket)) {
+        const newSocketList = socketList.filter((s) => s !== socket);
+        allSockets.set(key, newSocketList);
+        return;
+      }
+    }
+  });
+});
