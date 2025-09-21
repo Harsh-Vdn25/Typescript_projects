56e5b19d44fc4f241f332b3b8f5e663a55e663aa
Harsh-Vdn25 <hars.vdn16@gmail.com>
2025-09-21 19:53:46 +0530

    fixed typo & message bug

--- socketBE/src/index.ts
+++ socketBE/src/index.ts
@@ -7,10 +7,15 @@ let userCount=0;
 //roomId:string socket:WebSocket
 let allSockets=new Map<string,WebSocket[]>();
 
+interface messageType{
+    type:string;
+    message:string;
+}
+
 wss.on('connection',(socket,req)=>{
     socket.on('error',console.error);
 
-    const roomId=req?.url?.split('=')[1];
+    const roomId=req?.url?.split('=')[1]||'';
     if(!roomId){
         throw new Error("No room Id");
     }
@@ -20,16 +25,25 @@ wss.on('connection',(socket,req)=>{
     allSockets.set(roomId,roomSockets);
 
     socket.on('message',(e)=>{
-        for(const [key,userSocket] of allSockets.entries()){
+        const message=e.toString();
+        let messageInfo:messageType;
+        try{
+            messageInfo=JSON.parse(message);
+        }catch(err){
+            console.log("Not a Valid info",message);
+            return;
+        }
+        if(messageInfo?.type==='chat'){
+            for(const [key,userSocket] of allSockets.entries()){
            if (userSocket.includes(socket)){
-            userSocket.map(s=>s.send(e.toString()));
+            userSocket.map(s=>s.send(messageInfo.message));
            }
         }
+        }
     })
-
-    socket.on("disconnect",()=>{
-        const roomSockets=allSockets.get(roomId)||[];
-        roomSockets.filter(x=>x!=socket);
+    //Remove all the dead sockets 
+    socket.on("close",()=>{
+        const roomSockets=(allSockets.get(roomId)||[]).filter(x=>x!=socket);
         allSockets.set(roomId,roomSockets);
     })
 })
