6be3ec5417aa7e46254ee87b2e1896187249bde1
Harsh-Vdn25 <hars.vdn16@gmail.com>
2025-09-29 22:30:10 +0530

    removed TSXpractice file

--- TSXpractice/dist/controllers/itemcontroller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare const AddItem: (req: Request, res: Response) => Promise<void>;
-export declare const readItems: (res: Response) => Promise<void>;
-export declare const readItem: (req: Request, res: Response) => Promise<void>;
-export declare const removeItem: (req: Request, res: Response) => Promise<void>;
-export declare const updateItem: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=itemcontroller.d.ts.map
--- TSXpractice/dist/routes/itemroute.d.ts
+++
@@ -1,3 +0,0 @@
-declare const ItemRouter: import("express-serve-static-core").Router;
-export { ItemRouter };
-//# sourceMappingURL=itemroute.d.ts.map
